59d69ea261933f3baacf3e7affa5db09ef75da7c
meryemayhan <meryemayhan4@gmail.com>
2020-06-26 00:41:50 +0300

    Create 10001st-prime.js

---
+++ 10001st-prime.js
@@ -0,0 +1,20 @@
+Public static void main(string[] args) {
+                        Boolean[] primes = new boolean[10000000];
+                       Arrays.fill(primes, true);
+                       Primes[0]=primes[1]=false;
+                       For(int i=2;i<primes.lenhgt;++i){
+                                     İf(primes[i]){
+                                           For(int j=i+i;j<primes.lenght;j+i)
+                       Primes[j]=false;	
+}
+}
+                      İnt count = 0;
+                         For(int i=2;i<primes.lenght;++i){
+                             İf(primes[i]){
+                             ++count;
+                             İf(count== 10001)
+                      System.out.printIn(i);
+}
+}
+                      System.out.printIn(“Finish”);
+}
